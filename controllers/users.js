921b91e37a3777ffd4519301ed2ad4cb324cf4ea
denon1998 <nuwanthikasamarawila@gmail.com>
2021-10-07 00:07:21 +0530

    user controller added email

--- controllers/users.js
+++ controllers/users.js
@@ -12,8 +12,9 @@ router.route('/add').post((req, res) => {
     const username = req.body.username;
     const Type = req.body.Type;
     const password = req.body.password;
+    const email = req.body.email;
 
-    const newUser = new User({ username, Type, password, });
+    const newUser = new User({ username, Type, password,email });
 
     newUser.save()
         .then(() => res.json('User added!'))
